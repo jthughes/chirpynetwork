92a9f9a546721e1de0da65589cc1d2c68280d28b
Jeremy Hughes <jthughes@users.noreply.github.com>
2025-01-14 10:39:46 +1030

    Able to add and retrieve chirps

--- main.go
+++ main.go
@@ -55,7 +55,10 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", handlerReadiness)
 	serveMux.HandleFunc("GET /admin/metrics", apiCfg.handlerFileserverHits)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerResetUsers)
-	serveMux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	// serveMux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	serveMux.HandleFunc("POST /api/chirps", apiCfg.handlerNewChirp)
+	serveMux.HandleFunc("GET /api/chirps", apiCfg.handlerGetAllChirps)
+	serveMux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerGetChirpByID)
 	serveMux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
 	server := http.Server{
@@ -147,10 +150,124 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 
 	if len(test.Body) > 140 {
 		ResponseError(w, nil, "Chirp is too long", http.StatusBadRequest)
+		return
 	}
 
 	data, err := json.Marshal(responseSuccess{
 		CleanedBody: clean_message(test.Body),
+	})
+	SetJSONResponse(w, http.StatusOK, data, err)
+}
+
+func (cfg *apiConfig) handlerNewChirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body   string    `json:"body"`
+		UserId uuid.UUID `json:"user_id"`
+	}
+	type responseSuccess struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserId    uuid.UUID `json:"user_id"`
+	}
+
+	// Attempt to validate input json
+	decoder := json.NewDecoder(r.Body)
+	test := chirp{}
+	err := decoder.Decode(&test)
+	if err != nil {
+		ResponseError(w, err, "Error decoding chirp", http.StatusInternalServerError)
+		return
+	}
+
+	user, err := cfg.db.GetUserById(context.Background(), test.UserId)
+	if err != nil {
+		ResponseError(w, nil, "User does not exist", http.StatusBadRequest)
+		return
+	}
+
+	if len(test.Body) > 140 {
+		ResponseError(w, nil, "Chirp is too long", http.StatusBadRequest)
+		return
+	}
+
+	inserted, err := cfg.db.CreateChirp(context.Background(), database.CreateChirpParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Body:      test.Body,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		ResponseError(w, nil, "Unable to create chirp", http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(responseSuccess{
+		ID:        inserted.ID,
+		CreatedAt: inserted.CreatedAt,
+		UpdatedAt: inserted.UpdatedAt,
+		Body:      inserted.Body,
+		UserId:    inserted.UserID,
+	})
+	SetJSONResponse(w, http.StatusCreated, data, err)
+}
+
+func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	type Chirp struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserId    uuid.UUID `json:"user_id"`
+	}
+
+	chirps, err := cfg.db.GetAllChirps(context.Background())
+	if err != nil {
+		ResponseError(w, err, "Error retrieving chirps", http.StatusInternalServerError)
+		return
+	}
+	out := []Chirp{}
+	for _, item := range chirps {
+		out = append(out, Chirp{
+			ID:        item.ID,
+			CreatedAt: item.CreatedAt,
+			UpdatedAt: item.UpdatedAt,
+			Body:      item.Body,
+			UserId:    item.UserID,
+		})
+	}
+
+	data, err := json.Marshal(out)
+	SetJSONResponse(w, http.StatusOK, data, err)
+}
+
+func (cfg *apiConfig) handlerGetChirpByID(w http.ResponseWriter, r *http.Request) {
+	type Chirp struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserId    uuid.UUID `json:"user_id"`
+	}
+	requestUserID, err := uuid.Parse(r.PathValue("chirpID"))
+	if err != nil {
+		ResponseError(w, err, "Error parsing chirp id", http.StatusBadRequest)
+		return
+	}
+	dbChirp, err := cfg.db.GetChirpByID(context.Background(), requestUserID)
+	if err != nil {
+		ResponseError(w, err, "Chirp ID not found", http.StatusNotFound)
+		return
+	}
+
+	data, err := json.Marshal(Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserId:    dbChirp.UserID,
 	})
 	SetJSONResponse(w, http.StatusOK, data, err)
 }
